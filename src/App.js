b6be346aee222a39b4e566bbae2cd17da9b2912d
Lucas Carvalho <49245496+LucssCarvalho@users.noreply.github.com>
2020-03-26 23:53:42 -0300

    add fragment

--- src/App.js
+++ src/App.js
@@ -1,42 +1,62 @@
-import React from 'react';
+import React, { Component, Fragment } from 'react';
 import './App.css';
 import Tabela from './table.js'
+import Formulario from './Formulario';
 
-function App() {
+class App extends Component {
 
-  const autores = [
-    {
-      nome: 'Paulo',
-      livro: 'React',
-      preco: '1000'
-    },
-    {
-      nome: 'Daniel',
-      livro: 'Java',
-      preco: '800'
-    },
-    {
-      nome: 'Marcos',
-      livro: 'Designer',
-      preco: '700'
-    },
-    {
-      nome: 'Bruno',
-      livro: 'DevOps',
-      preco: '600'
-    },
-    {
-      nome: 'Lucas',
-      livro: 'Flutter',
-      preco: '1200'
-    },
-  ]
+  state = {
+    autores: [
+      {
+        nome: 'Paulo',
+        livro: 'React',
+        preco: '1000'
+      },
+      {
+        nome: 'Daniel',
+        livro: 'Java',
+        preco: '800'
+      },
+      {
+        nome: 'Marcos',
+        livro: 'Designer',
+        preco: '700'
+      },
+      {
+        nome: 'Bruno',
+        livro: 'DevOps',
+        preco: '600'
+      },
+      {
+        nome: 'Lucas',
+        livro: 'Flutter',
+        preco: '1200'
+      },
+    ],
+  };
 
-  return (
-    <div className="App">
-      <Tabela autores={autores} />
-    </div>
-  );
+  removeAutor = index => {
+
+    const { autores } = this.state;
+
+    this.setState(
+      {
+        autores: autores.filter((autor, posAtual) => {
+          return posAtual !== index;
+        }),
+      }
+    );
+  }
+
+  render() {
+    return (
+      <Fragment>
+        <Tabela autores={this.state.autores} removeAutor={this.removeAutor} />
+        <Formulario />
+      </Fragment>
+    );
+  }
+
 }
 
 export default App;

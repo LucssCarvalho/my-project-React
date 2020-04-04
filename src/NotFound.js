476253c6f63097ae865fe80f6e074c8ea2df300b
Lucas Carvalho <49245496+LucssCarvalho@users.noreply.github.com>
2020-04-04 11:52:29 -0300

    create page notfound

---
+++ src/NotFound.js
@@ -0,0 +1,13 @@
+import React, { Fragment } from 'react';
+import Header from './Header';
+
+const NotFound = () => {
+    return (
+        <Fragment>
+            <Header />
+            <h2>Página não encontrada</h2>
+
+        </Fragment>
+    );
+}
+export default NotFound;

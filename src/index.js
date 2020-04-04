f591c4093770063c66fb566dfd56f402e5af4bdc
Lucas Carvalho <49245496+LucssCarvalho@users.noreply.github.com>
2020-04-04 11:51:20 -0300

    create routes

--- src/index.js
+++ src/index.js
@@ -3,10 +3,23 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import Sobre from './Sobre';
+import Autores from './Autores';
+import Livros from './Livros';
+import NotFound from './NotFound';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Switch>
+        <Route path='/' exact={true} component={App} />
+        <Route path='/sobre' component={Sobre} />
+        <Route path='/livros' component={Livros} />
+        <Route path='/autores' component={Autores} />
+        <Route component={NotFound} />
+      </Switch>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );

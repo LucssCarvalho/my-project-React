cb04b4cb9065cbe2f8168e6be286c22f25851dc9
Lucas Carvalho <49245496+LucssCarvalho@users.noreply.github.com>
2020-04-04 11:50:22 -0300

    create autores page

---
+++ src/Autores.js
@@ -0,0 +1,13 @@
+import React, { Fragment } from 'react';
+import Header from './Header';
+
+const Autores = () => {
+    return (
+        <Fragment>
+            <Header />
+            <h2>Autores</h2>
+
+        </Fragment>
+    );
+}
+export default Autores;

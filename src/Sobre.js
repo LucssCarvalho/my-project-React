34e52eddbda6b305ca1cc9228689bce350cd50f1
Lucas Carvalho <49245496+LucssCarvalho@users.noreply.github.com>
2020-04-04 11:52:40 -0300

    create page sobre

---
+++ src/Sobre.js
@@ -0,0 +1,13 @@
+import React, { Fragment } from 'react';
+import Header from './Header';
+
+const Sobre = () => {
+    return (
+        <Fragment>
+            <Header />
+            <h2>Sobre</h2>
+
+        </Fragment>
+    );
+}
+export default Sobre;

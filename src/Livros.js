14d1032123c24dedf007089b321914046fc0db88
Lucas Carvalho <49245496+LucssCarvalho@users.noreply.github.com>
2020-04-04 11:51:56 -0300

    create page livros

---
+++ src/Livros.js
@@ -0,0 +1,13 @@
+import React, { Fragment } from 'react';
+import Header from './Header';
+
+const Livros = () => {
+    return (
+        <Fragment>
+            <Header />
+            <h2>Livros</h2>
+
+        </Fragment>
+    );
+}
+export default Livros;
